eae5ceb3abb0ee5293edd85bff2bdba76f99eead
Monique Robinson <monique.robinson2@gmail.com>
2018-04-03 21:47:10 -0400

    Got the spotify npm to work and the request npm to work

--- liri.js
+++ liri.js
@@ -22,7 +22,7 @@ switch (command) {
     getSong();
     break;
   case "movie-this":
-    movies();
+    getMovie();
     break;
   case "do-what-it-says":
     random();
@@ -49,18 +49,39 @@ function getTweets() {
 };
 
 
-//need to get the right data
+//need to get the right data for album
 function getSong() {
   var song = process.argv.slice(3).join(' ');
   spotify.search({
     type: 'track',
     query: song,
-    limit:3
+    limit:1
   }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
     // console.log(JSON.stringify(data, null, 2));
-    console.log(JSON.stringify(data.tracks[0], null, 2));
+    console.log("Artist Name: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+    console.log("Song Name: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+    console.log("Preview: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
   });
+}
+
+function getMovie() {
+  var movieName = process.argv.slice(3).join(" ");
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+request(queryUrl, function (err, response, data) {
+  if (!err && response.statusCode === 200) {
+    console.log(data);
+    console.log("Movie Title: " + JSON.parse(data).Title);
+    console.log("Released in: " + JSON.parse(data).Year);
+    console.log("IMDB Rating: " + JSON.parse(data).imdbRating);
+    console.log(JSON.parse(data).Ratings[1]);
+    console.log("Produced in: " + JSON.parse(data).Country);
+    console.log("Language: " + JSON.parse(data).Language);
+    console.log("Plot: " + JSON.parse(data).Plot);
+    console.log("Actors: " + JSON.parse(data).Actors);
+  }
+});
 }
