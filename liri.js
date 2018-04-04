80bafc81ebf75519154e460e409fbf6c9b929ca1
Monique Robinson <monique.robinson2@gmail.com>
2018-04-03 22:41:04 -0400

    Added the fs package

--- liri.js
+++ liri.js
@@ -98,3 +98,18 @@ function getMovie(input) {
     }
   });
 }
+
+function random() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+    // Print the contents of data
+    console.log(data);
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
+  });
+}
