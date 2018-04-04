e88a019d0d54e6d28502bbd788cdd0585ae3f8b2
Monique Robinson <monique.robinson2@gmail.com>
2018-04-03 22:31:26 -0400

    Spotify works completely

--- liri.js
+++ liri.js
@@ -14,15 +14,17 @@ var client = new Twitter(keys.twitter);
 
 var command = process.argv[2];
 
+var input = process.argv.slice(3).join(' ');
+
 switch (command) {
   case "my-tweets":
     getTweets();
     break;
   case "spotify-this-song":
-    getSong();
+    getSong(input);
     break;
   case "movie-this":
-    getMovie();
+    getMovie(input);
     break;
   case "do-what-it-says":
     random();
@@ -31,6 +33,7 @@ switch (command) {
     console.log("You didn't put in a proper search term. PLease use 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'.")
 }
 
+//function to return the last 20 tweets using twitter npm
 function getTweets() {
   var params = {
     screen_name: 'moMoneyMoRamen',
@@ -49,13 +52,18 @@ function getTweets() {
 };
 
 
-//need to get the right data for album
-function getSong() {
-  var song = process.argv.slice(3).join(' ');
+//allow input with "spotify-this-song"
+function getSong(input) {
+  if (input === '') {
+    song = "The Sign Ace of Base"
+  } else {
+    song = input;
+  }
+  //search the spotify API
   spotify.search({
     type: 'track',
     query: song,
-    limit:1
+    limit: 1
   }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
@@ -64,24 +72,30 @@ function getSong() {
     console.log("Artist Name: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
     console.log("Song Name: " + JSON.stringify(data.tracks.items[0].name, null, 2));
     console.log("Preview: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+    console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
   });
 }
 
-function getMovie() {
+function getMovie(input) {
+  if (input === '') {
+    movieName = "Mr. Nobody";
+  } else {
+    movieName = input;
+  }
   var movieName = process.argv.slice(3).join(" ");
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-request(queryUrl, function (err, response, data) {
-  if (!err && response.statusCode === 200) {
-    console.log(data);
-    console.log("Movie Title: " + JSON.parse(data).Title);
-    console.log("Released in: " + JSON.parse(data).Year);
-    console.log("IMDB Rating: " + JSON.parse(data).imdbRating);
-    console.log(JSON.parse(data).Ratings[1]);
-    console.log("Produced in: " + JSON.parse(data).Country);
-    console.log("Language: " + JSON.parse(data).Language);
-    console.log("Plot: " + JSON.parse(data).Plot);
-    console.log("Actors: " + JSON.parse(data).Actors);
-  }
-});
+  request(queryUrl, function (err, response, data) {
+    if (!err && response.statusCode === 200) {
+      console.log(data);
+      console.log("Movie Title: " + JSON.parse(data).Title);
+      console.log("Released in: " + JSON.parse(data).Year);
+      console.log("IMDB Rating: " + JSON.parse(data).imdbRating);
+      // console.log(JSON.parse(data).Ratings[1]);
+      console.log("Produced in: " + JSON.parse(data).Country);
+      console.log("Language: " + JSON.parse(data).Language);
+      console.log("Plot: " + JSON.parse(data).Plot);
+      console.log("Actors: " + JSON.parse(data).Actors);
+    }
+  });
 }
